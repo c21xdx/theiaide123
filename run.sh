18c5194cc3c54e03da8a3c600bc907b2178a56f0
c21xdx <c21xdx@gmail.com>
2020-02-17 20:08:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+sed -i "s/frps_ip/$FRPS_IP/" /etc/supervisord.conf
+sed -i "s/f_domain/$DOMAIN/" /etc/supervisord.conf
+sed -i "s/f_user/$USER/" /etc/supervisord.conf
+sed -i "s/f_passwd/$PASSWD/" /etc/supervisord.conf
+supervisord -c /etc/supervisord.conf
 node /home/theia/src-gen/backend/main.js /home/project --hostname=0.0.0.0
 /home/frp/frp_0.31.2_linux_amd64/frpc -c /home/frp/frpc.ini
