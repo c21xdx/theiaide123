ee347cce0e440f24f35c0832c8eb4ba8838709e8
c21xdx <c21xdx@gmail.com>
2020-02-17 20:22:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sed -i "s/frps_ip/$FRPS_IP/" /home/frp/frpc.ini
+sed -i "s/frps_port/$FRPS_PORT/" /home/frp/frpc.ini
 sed -i "s/f_domain/$DOMAIN/" /home/frp/frpc.ini
 sed -i "s/f_user/$USER/" /home/frp/frpc.ini
 sed -i "s/f_passwd/$PASSWD/" /home/frp/frpc.ini
