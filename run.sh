fe01ba43610ba9148cb5866164b208302d3e81b2
c21xdx <c21xdx@gmail.com>
2020-02-17 20:19:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sed -i "s/frps_ip/$FRPS_IP/" /etc/supervisord.conf
-sed -i "s/f_domain/$DOMAIN/" /etc/supervisord.conf
-sed -i "s/f_user/$USER/" /etc/supervisord.conf
-sed -i "s/f_passwd/$PASSWD/" /etc/supervisord.conf
+sed -i "s/frps_ip/$FRPS_IP/" /home/frp/frpc.ini
+sed -i "s/f_domain/$DOMAIN/" /home/frp/frpc.ini
+sed -i "s/f_user/$USER/" /home/frp/frpc.ini
+sed -i "s/f_passwd/$PASSWD/" /home/frp/frpc.ini
 supervisord -c /etc/supervisord.conf
